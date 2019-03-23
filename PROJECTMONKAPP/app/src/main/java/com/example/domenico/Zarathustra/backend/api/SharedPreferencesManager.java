3e9e4c58ff8926ba7c465ddac090b2f35d035402
Marian Gabriel Neacsu <ncs.gabriel.w@gmail.com>
2019-03-23 18:22:34 +0100

    SharedPreferencesManager start

---
+++ PROJECTMONKAPP/app/src/main/java/com/example/domenico/Zarathustra/backend/api/SharedPreferencesManager.java
@@ -0,0 +1,11 @@
+package com.example.domenico.Zarathustra.backend.api;
+
+public class SharedPreferencesManager {
+
+    public static void set(String name, String value) {
+
+    }
+    public static String get (String name){
+        return null;
+    }
+}
