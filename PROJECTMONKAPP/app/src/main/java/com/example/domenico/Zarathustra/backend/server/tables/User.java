2c5b79d21f92c0e904a4242e6701ec10807455f1
Francesco Ruta <gongciao@gmail.com>
2019-03-23 19:26:41 +0100

    User

---
+++ PROJECTMONKAPP/app/src/main/java/com/example/domenico/Zarathustra/backend/server/tables/User.java
@@ -0,0 +1,23 @@
+package com.example.domenico.Zarathustra.backend.server;
+
+public class User {
+	private int imageId;
+	private String username;
+	private String password;
+	
+	public User(int imageId, String username, String password) {
+		this.imageId = imageId;
+		this.username = username;
+		this.password = password;
+	}
+	public int getImageId() {
+		return imageId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	
+}
