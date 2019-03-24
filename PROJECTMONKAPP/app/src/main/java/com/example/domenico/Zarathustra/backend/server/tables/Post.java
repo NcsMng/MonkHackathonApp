5e73c7ba5bb6608e33ec6b9c5e6467df141a3cce
Simone Basile <basile.simone@libero.it>
2019-03-24 05:20:18 +0100

    Sostituzione vecchio sistema db

--- PROJECTMONKAPP/app/src/main/java/com/example/domenico/Zarathustra/backend/server/tables/Post.java
+++ PROJECTMONKAPP/app/src/main/java/com/example/domenico/Zarathustra/backend/server/tables/Post.java
@@ -1,55 +1,44 @@
 package com.example.domenico.Zarathustra.backend.server.tables;
 
-import android.arch.persistence.room.PrimaryKey;
+
+import com.example.domenico.Zarathustra.backend.server.tables.user.User;
 
 import java.io.Serializable;
-import java.time.Instant;
 import java.util.Date;
 
-
 public abstract class Post implements Serializable {
-    @PrimaryKey(autoGenerate = true)
-    private long id;
-
+    private static final long serialVersionUID = -1651968622414322439L;
     private String title;
-    private long authorId;
+    private User author;
     private String content;
-    private long submittingTime;
-    public Post(){}
-	public Post(String title, long authorId, String content, long submittingTime){
+    private Date submittingTime;
+
+	public Post(String title, User author, String content, Date submittingTime){
 		this.content=content;
 		this.title = title;
-		this.authorId = authorId;
+		this.author = author;
 		this.submittingTime = submittingTime;
 	}
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setAuthorId(long authorId) {
-        this.authorId = authorId;
+    public void setAuthor(User author) {
+        this.author = author;
     }
 
     public void setContent(String content) {
         this.content = content;
     }
 
-    public void setSubmittingTime(long submittingTime) {
+    public void setSubmittingTime(Date submittingTime) {
         this.submittingTime = submittingTime;
     }
 
 	public String getTitle(){ return this.title;}
-    public long getAuthorId(){ return this.authorId;}
+    public User getAuthor(){ return this.author;}
     public String getContent(){ return this.content;}
-    public long getSubmittingTime(){ return this.submittingTime;}
-    public Date getSubmittingTimeAsDate(){ return new Date(submittingTime);}
+    public Date getSubmittingTime(){ return this.submittingTime;}
+    public Date getSubmittingTimeAsDate(){ return this.submittingTime;}
 }
--- PROJECTMONKAPP/app/src/main/java/com/example/domenico/Zarathustra/backend/server/tables/TextPost.java
+++ PROJECTMONKAPP/app/src/main/java/com/example/domenico/Zarathustra/backend/server/tables/TextPost.java
@@ -1,15 +1,13 @@
 package com.example.domenico.Zarathustra.backend.server.tables;
 
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.ForeignKey;
-import android.arch.persistence.room.Ignore;
+import com.example.domenico.Zarathustra.backend.server.tables.user.User;
 
-@Entity(foreignKeys = @ForeignKey(entity = User.class,parentColumns = "id",childColumns = "authorId",onDelete = ForeignKey.CASCADE))
+import java.util.Date;
+
 public class TextPost extends Post{
-	private static final long serialVersionUID = 2615122975L;
-    @Ignore
-	public TextPost(String title, long author, String content, long submittingTime){
+	private static final long serialVersionUID = -2572159124006482276L;
+
+	public TextPost(String title, User author, String content, Date submittingTime){
 		super(title, author, content, submittingTime);
 	}
-	public TextPost(){}
 }
