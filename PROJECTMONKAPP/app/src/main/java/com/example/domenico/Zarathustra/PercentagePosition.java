51f3346b7fb417d476e9b50d785ae48dba0afd86
Matteo <matteoaleo99@gmail.com>
2019-03-24 04:57:33 +0100

    Beacons

---
+++ PROJECTMONKAPP/app/src/main/java/com/example/domenico/Zarathustra/PercentagePosition.java
@@ -0,0 +1,10 @@
+package com.example.beacon;
+
+public class PercentagePosition {
+    public final double x, y;
+
+    public PercentagePosition(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
