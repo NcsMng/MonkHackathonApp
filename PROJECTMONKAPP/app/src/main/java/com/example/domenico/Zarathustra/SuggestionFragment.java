91f164fd9e56a04d746091d5ffc72ec3ec2d6073
Domenico <razorpsn7@gmail.com>
2019-03-24 07:25:16 +0100

    Fragment e Bacheca

--- PROJECTMONKAPP/app/src/main/java/com/example/domenico/Zarathustra/SuggestionFragment.java
+++ PROJECTMONKAPP/app/src/main/java/com/example/domenico/Zarathustra/SuggestionFragment.java
@@ -1,4 +1,19 @@
 package com.example.domenico.Zarathustra;
 
-public class SuggestionFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SuggestionFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.row_suggestions,container,false);
+        return view;
+    }
 }
