7e878aee71e06e2b6d3136577cbd96af52488665
Simone Basile <basile.simone@libero.it>
2019-03-24 02:19:28 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ PROJECTMONKAPP/app/src/main/java/com/example/domenico/Zarathustra/backend/api/Connection/RequestType.java
@@ -0,0 +1,8 @@
+package com.example.domenico.Zarathustra.backend.api.Connection;
+
+import java.io.Serializable;
+
+public enum RequestType implements Serializable {
+	LOGIN;
+	private static final long serialVersionUID = -7759509376536906193L;
+}
